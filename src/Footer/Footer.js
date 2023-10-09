14a1b8f7804cabb915681225bb43898a6bb036e7
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-10-09 10:58:38 +0100

    fin copyright footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -6,28 +6,37 @@ import { SocialIcon } from 'react-social-icons'
 
 export default function Footer() {
   return (
+
     <div className='footer-container'>
+      <div className='top-footer'>
 
-      <div className='abonnement'>
-        <h4>Abonnez-vous à nos réseaux sociaux</h4>
-        <span><SocialIcon url="https://www.instagram.com/phenixdeals/" />  </span>
-        <span style={{ marginLeft: 20 }}><SocialIcon url="https://web.facebook.com/profile.php?id=100090243464213" />  </span>
+        <div className='abonnement'>
+          <h4>Abonnez-vous à nos réseaux sociaux</h4>
+          <span><SocialIcon url="https://www.instagram.com/phenixdeals/" />  </span>
+          <span style={{ marginLeft: 20 }}><SocialIcon url="https://web.facebook.com/profile.php?id=100090243464213" />  </span>
+        </div>
+
+
+        <div className='contact-footer'>
+          <h4 style={{ color: "black" }}>Contact</h4>
+          <div>
+            <span><FontAwesomeIcon icon={faHouse} style={{ color: "#404544" }} /> </span>
+            <span>Casablanca, Maroc</span>
+          </div>
+          <div style={{ marginTop: 20 }}>
+            <span><FontAwesomeIcon icon={faEnvelope} style={{ color: "#404544" }} /> </span>
+            <span> phenix.deals@gmail.com</span>
+          </div>
+        </div>
+
       </div>
 
-
-      <div className='contact-footer'>
-        <h4 style={{ color: "black" }}>Contact</h4>
-        <div>
-          <span><FontAwesomeIcon icon={faHouse} style={{ color: "#404544" }} /> </span>
-          <span>Casablanca, Maroc</span>
-        </div>
-        <div style={{ marginTop: 20 }}>
-          <span><FontAwesomeIcon icon={faEnvelope} style={{ color: "#404544" }} /> </span>
-          <span> phenix.deals@gmail.com</span>
-        </div>
+      <div className='copyright'>
+        <span>Copyright © - Phenixdeals </span>
       </div>
 
-      
     </div>
+
+
   )
 }
