e915047c8651484e83c8257e379150aa29cfd5c7
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-10-09 10:34:38 +0100

    footer changed

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -10,19 +10,10 @@ export default function Footer() {
 
       <div className='abonnement'>
         <h4>Abonnez-vous à nos réseaux sociaux</h4>
+        <span><SocialIcon url="https://www.instagram.com/phenixdeals/" />  </span>
+        <span style={{ marginLeft: 20 }}><SocialIcon url="https://web.facebook.com/profile.php?id=100090243464213" />  </span>
       </div>
 
-      <div className='phenix-mot'>
-        <h4>Phenixdeals.com est un espace où vous pouvez mettre en vente vos objets d'art, de décoration ou vos bijoux</h4>
-      </div>
-
-      <div className='liens-footer'>
-        <h4 style={{ color: "black" }}>Liens</h4>
-        <h5>Tableaux</h5>
-        <h5>Décoration</h5>
-        <h5>Bijoux</h5>
-        <h5>Livres</h5>
-      </div>
 
       <div className='contact-footer'>
         <h4 style={{ color: "black" }}>Contact</h4>
@@ -36,10 +27,7 @@ export default function Footer() {
         </div>
       </div>
 
-      <div className='reseaux-footer'>
-        <span><SocialIcon url="https://www.instagram.com/phenixdeals/" />  </span>
-        <span style={{ marginLeft: 20 }}><SocialIcon url="https://web.facebook.com/profile.php?id=100090243464213" />  </span>
-      </div>
+      
     </div>
   )
 }
