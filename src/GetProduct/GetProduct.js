8458a0f082b6eea154e7b6c401be50d944a8598a
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-10-19 10:45:02 +0100

    image map produits get fonctionne

--- src/GetProduct/GetProduct.js
+++ src/GetProduct/GetProduct.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 
 import "./getProduct.css"
 
+
 export default function GetProduct() {
 
   const [productObject, setProductObject] = useState([]); //State variable ou on va store tous les objets représentants les produits
   const [error, setError] = useState('');
+  
   const [productName, setProductName] = useState('');//State variable qui va store la value de l'input et qui doit changer en fonction de ce qu'on écrit
-
-
+  
   const handleProductName = (e) => setProductName(e.target.value); //ca c'est l'event handler qui fait que la state productName a pour valeur la value de l'input
 
   useEffect(() => {
@@ -48,10 +49,13 @@ export default function GetProduct() {
           <div>
             <ul>
               {filteredProducts.map((item) => (
+
+                
                 <li key={item.id}>
                   <h2>{item.nom}</h2>
                   <img
-                    src={item.imageUrl}
+                    className='all-products-image'
+                    src={`http://localhost:3005/${item.imageUrl}`}//On store le path de l'image dans la database,, donc c'est ce qu'il faut chercher
                     alt={item.nom}
                   />
                 </li>
