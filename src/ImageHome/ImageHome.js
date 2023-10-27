a7edad410edc57d06656ca29b3521c6456c55ed1
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-10-28 00:16:22 +0100

    Animation imagehome

--- src/ImageHome/ImageHome.js
+++ src/ImageHome/ImageHome.js
@@ -1,13 +1,29 @@
 import { Link } from 'react-router-dom'
 import './imageHome.css'
 
+import Aos from 'aos';
+import 'aos/dist/aos.css'
+import { useEffect } from 'react';
+
 
 
 
 export default function ImageHome() {
+
+  
+  useEffect(() => {
+    Aos.init({
+      once: true,
+      offset: 100,
+      duration: 1500,
+      easing: 'ease-in-out',
+    
+    });
+  })
+
   return (
     <div className='all-img-home'>
-      <div className='div-img-home'>
+      <div className='div-img-home' data-aos="fade-left">
         <Link to="/concept">
           <img src="/imagesTableaux/salon-galerie.jpg" alt="home-slide" className="image-home" />
         </Link>
