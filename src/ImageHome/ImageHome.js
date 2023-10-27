98986fe3fa1de4f48509fda21f5be19979132343
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-10-28 00:31:16 +0100

    suppression animation imageHome

--- src/ImageHome/ImageHome.js
+++ src/ImageHome/ImageHome.js
@@ -1,10 +1,7 @@
 import { Link } from 'react-router-dom'
 import './imageHome.css'
 
-import Aos from 'aos';
-import 'aos/dist/aos.css'
 
-import { useEffect } from 'react';
 
 
 
@@ -12,19 +9,9 @@ import { useEffect } from 'react';
 export default function ImageHome() {
 
   
-  useEffect(() => {
-    Aos.init({
-      once: true,
-      offset: 100,
-      duration: 1500,
-      easing: 'ease-in-out',
-    
-    });
-  })
-
   return (
     <div className='all-img-home'>
-      <div className='div-img-home' data-aos="fade-left">
+      <div className='div-img-home'>
         <Link to="/concept">
           <img src="/imagesTableaux/salon-galerie.jpg" alt="home-slide" className="image-home" />
         </Link>
