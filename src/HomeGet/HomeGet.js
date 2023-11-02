92011a6f5fbafe56475682f61a6a7f3f39854fe8
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-11-02 23:53:34 +0100

    création input-google components pr props

--- src/HomeGet/HomeGet.js
+++ src/HomeGet/HomeGet.js
@@ -10,6 +10,8 @@ import Aos from 'aos';
 import 'aos/dist/aos.css'
 
 import './homeGet.css'
+import InputGoogle from '../PropsComponents/InputGoogle';
+
 
 
 
@@ -22,9 +24,9 @@ export default function HomeGet() {
   const [productObject, setProductObject] = useState([]); //State variable ou on va store tous les objets représentants les produits
   const [error, setError] = useState('');
 
-  const [productName, setProductName] = useState('');//State variable qui va store la value de l'input et qui doit changer en fonction de ce qu'on écrit
+  const [productType, setProductType] = useState('');//Pour searchBar: State variable qui va store la value de l'input et qui doit changer en fonction de ce qu'on écrit
 
-  const handleProductName = (e) => setProductName(e.target.value); //ca c'est l'event handler qui fait que la state productName a pour valeur la value de l'input
+  const handleProductType = (e) => setProductType(e.target.value); //ca c'est l'event handler qui fait que la state productName a pour valeur la value de l'input
 
 
   useEffect(() => {
@@ -44,7 +46,7 @@ export default function HomeGet() {
 
 
   const filteredProducts = productObject.filter((item) =>
-    item.type.toLowerCase().includes(productName.toLowerCase())
+    item.type.toLowerCase().includes(productType.toLowerCase())
   );
 
   useEffect(() => {
@@ -67,29 +69,7 @@ export default function HomeGet() {
         </h1>
       </Link>
 
-      <div className='input-home'>
-
-        <div class="searchbar">
-          <div class="searchbar-wrapper">
-            <div class="searchbar-left">
-              <div class="search-icon-wrapper">
-                <span class="search-icon searchbar-icon">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z">
-                    </path>
-                  </svg>
-                </span>
-              </div>
-            </div>
-
-            <div class="searchbar-center">
-              <div class="searchbar-input-spacer"></div>
-              <input value={productName} onChange={handleProductName} class="searchbar-input" title="Search" role="combobox" maxlength="2048" name="q" autocapitalize="off" autocomplete="off" placeholder="tableau, déco, bijou, livre" type="text" />
-            </div>
-          </div>
-        </div>
-
-      </div>
+      <InputGoogle value={productType} onChange={handleProductType} />
 
       <div>
         {error ? (<p>Error: {error}</p>) : (
