620b8d58b8582438be0c85a0dd0dc6866cc18043
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-11-09 18:52:36 +0100

    évolution footer style

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,6 +1,6 @@
 import './footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
 
 import { SocialIcon } from 'react-social-icons'
 
@@ -11,17 +11,21 @@ export default function Footer() {
       <div className='top-footer'>
 
         <div className='abonnement'>
-          <h4>Abonnez-vous à nos réseaux sociaux</h4>
+          <h4>Abonnez-vous à nos réseaux sociaux: </h4>
           <span><SocialIcon url="https://www.instagram.com/phenixdeals/" target="_blank" />  </span>
           <span style={{ marginLeft: 20 }}><SocialIcon url="https://web.facebook.com/profile.php?id=100090243464213" target="_blank" />  </span>
         </div>
 
 
         <div className='contact-footer'>
-          <h4 style={{ color: "black" }}>Contact</h4>
+          <h4 style={{ color: "black" }}>Contact: </h4>
           <div>
             <span><FontAwesomeIcon icon={faHouse} style={{ color: "#404544" }} /> </span>
             <span>Casablanca, Maroc</span>
+          </div>
+          <div style={{ marginTop: 20 }}>
+            <span> <FontAwesomeIcon icon={faPhone} style={{ color: "#404544" }} /> </span>
+            <span> 06-19-63-53-36 </span>
           </div>
           <div style={{ marginTop: 20 }}>
             <span><FontAwesomeIcon icon={faEnvelope} style={{ color: "#404544" }} /> </span>
